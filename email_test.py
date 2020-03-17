ee3f13c5d1fd066d9dec6c966b91739f75f15609
willi79 <willigeraldy@gmail.com>
2020-03-17 15:57:12 +0700

    dummy_thing

--- email_test.py
+++ email_test.py
@@ -1,12 +1,15 @@
 import smtplib
 
-server = smtplib.SMTP('smtp.mailgun.org', 587)
-server.ehlo()
-server.starttls()
-server.ehlo()
-server.login('postmaster@sandbox9a81f6ab2bed4f17801a176da0ad5b42.mailgun.org', 'b64eb177bc7caa81f841ef64fa43b99b-1df6ec32-d0c0f265')
-subject = 'Email Test'
-body = "Test"
-msg = f"Subject: {subject}\n\n{body}"
-server.sendmail('postmaster@sandbox9a81f6ab2bed4f17801a176da0ad5b42.mailgun.org','willigeraldy@gmail.com',msg)
-server.quit()
+i=0
+while(i<5)
+    server = smtplib.SMTP('smtp.mailgun.org', 587)
+    server.ehlo()
+    server.starttls()
+    server.ehlo()
+    server.login('postmaster@sandbox9a81f6ab2bed4f17801a176da0ad5b42.mailgun.org', 'b64eb177bc7caa81f841ef64fa43b99b-1df6ec32-d0c0f265')
+    subject = 'Email Test'
+    body = "Test"
+    msg = f"Subject: {subject}\n\n{body}"
+    server.sendmail('postmaster@sandbox9a81f6ab2bed4f17801a176da0ad5b42.mailgun.org','willigeraldy@gmail.com',msg)
+    server.quit()
+    time.sleep(100)
