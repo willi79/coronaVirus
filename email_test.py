0595e54e19373a70dd443e2ad5124c1966d47586
willi79 <willigeraldy@gmail.com>
2020-03-18 09:32:21 +0700

    fix

--- email_test.py
+++ email_test.py
@@ -9,7 +9,7 @@ while(i<5):
     server.login('postmaster@sandbox9a81f6ab2bed4f17801a176da0ad5b42.mailgun.org', 'b64eb177bc7caa81f841ef64fa43b99b-1df6ec32-d0c0f265')
     subject = 'Email Test'
     body = "Test"
-    msg = f"Subject: {subject}\n\n{body}"
+    msg = "Subject: None"
     server.sendmail('postmaster@sandbox9a81f6ab2bed4f17801a176da0ad5b42.mailgun.org','willigeraldy@gmail.com',msg)
     server.quit()
     time.sleep(100)
